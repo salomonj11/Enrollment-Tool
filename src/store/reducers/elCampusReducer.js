f96b9fe73fee79d5221c7fa1e88e8f9281355f5e
Jesus Salomon <salomonj1121@gmail.com>
2022-09-01 15:52:01 -0500

    finished both student and campus reducers

--- src/store/reducers/elCampusReducer.js
+++ src/store/reducers/elCampusReducer.js
@@ -5,29 +5,33 @@ const CREATE_ONE_CAMPUS = 'CREATE_ONE_CAMPUS';
 const DELETE_CAMPUS = 'DELETE_CAMPUS';
 const UPDATE_CAMPUS = 'UPDATE_CAMPUS';
 
-export const elSetCampuses = (campuses) => {
+const elSetCampuses = (campuses) => {
   return {
     type: SET_CAMPUSES,
     campuses,
   };
 };
 
-export const elCreateOneCampus = (campus) => {
+const elCreateOneCampus = (campus) => {
   return {
     type: CREATE_ONE_CAMPUS,
     campus,
   };
 };
 
-export const elUpdateCampus = (campus) => ({
-  type: UPDATE_CAMPUS,
-  campus,
-});
+const elUpdateCampus = (campus) => {
+  return {
+    type: UPDATE_CAMPUS,
+    campus,
+  };
+};
 
-export const elDeleteCampus = (id) => ({
-  type: DELETE_CAMPUS,
-  id,
-});
+const elDeleteCampus = (campus) => {
+  return {
+    type: DELETE_CAMPUS,
+    campus,
+  };
+};
 
 //export stuff below //
 
--- src/store/reducers/elStudentReducers.js
+++ src/store/reducers/elStudentReducers.js
@@ -1,19 +1,95 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  gpa: '',
-  imageUrl: '',
+const SET_STUDENTS = 'SET_STUDENTS';
+const CREATE_ONE_STUDENT = 'CREATE_ONE_STUDENT';
+const DELETE_STUDENT = 'DELETE_STUDENT';
+const UPDATE_STUDENT = 'UPDATE_STUDENT';
+
+const elSetStudents = (students) => {
+  return {
+    type: SET_STUDENTS,
+    students,
+  };
 };
 
-export const myStudentsSlice = createSlice({
-  name: 'campus',
-  reducers: {},
-  initialState: {
-    value: initialState,
-  },
-});
+const elCreateOneStudent = (student) => {
+  return {
+    type: CREATE_ONE_STUDENT,
+    student,
+  };
+};
 
-export default myStudentsSlice.reducer;
+const elUpdateStudent = (student) => {
+  return {
+    type: UPDATE_STUDENT,
+    student,
+  };
+};
+
+const elDeleteStudent = (student) => {
+  return {
+    type: DELETE_STUDENT,
+    student,
+  };
+};
+
+//export stuff below //
+
+function elStudentReducer(state = [], action) {
+  switch (action.type) {
+    case SET_STUDENTS:
+      return action.students;
+    case CREATE_ONE_STUDENT:
+      return [...state, action.student];
+    case UPDATE_STUDENT:
+      return state.map((student) => {
+        return student.id === action.student.id
+          ? action.student
+          : student;
+      });
+    case DELETE_STUDENT:
+      return state.filter(
+        (student) => student.id !== action.student.id
+      );
+    default:
+      return state;
+  }
+}
+
+export const deleteStudent = (student) => {
+  return async (dispatch) => {
+    const { data: deleted } = await axios.delete(
+      `/api/students/${student.id}`
+    );
+    dispatch(elDeleteStudent(deleted));
+  };
+};
+
+export const updateStudent = (student) => {
+  return async (dispatch) => {
+    const { data: updated } = await axios.put(
+      `/api/students/${student.id}`,
+      student
+    );
+    dispatch(elUpdateStudent(updated));
+  };
+};
+
+export const createOneStudent = (student) => {
+  return async (dispatch) => {
+    const { data: created } = await axios.post(
+      '/api/students',
+      student
+    );
+    dispatch(elCreateOneStudent(created));
+  };
+};
+
+export const getStudents = () => {
+  return async (dispatch) => {
+    const { data: students } = await axios.get('/api/students');
+    dispatch(elSetStudents(students));
+  };
+};
+
+export default elStudentReducer;
