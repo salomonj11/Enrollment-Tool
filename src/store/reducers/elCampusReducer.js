9f347583e30ffd29570821c79ca68e7ee12a240b
Jesus Salomon <salomonj1121@gmail.com>
2022-09-01 14:59:57 -0500

    finished campus reducer, removed action creators

--- src/store/reducers/elCampusReducer.js
+++ src/store/reducers/elCampusReducer.js
@@ -28,3 +28,62 @@ export const elDeleteCampus = (id) => ({
   type: DELETE_CAMPUS,
   id,
 });
+
+//export stuff below //
+
+function elCampusReducer(state = [], action) {
+  switch (action.type) {
+    case SET_CAMPUSES:
+      return action.campuses;
+    case CREATE_ONE_CAMPUS:
+      return [...state, action.campus];
+    case UPDATE_CAMPUS:
+      return state.map((campus) => {
+        return campus.id === action.campus.id
+          ? action.campus
+          : campus;
+      });
+    case DELETE_CAMPUS:
+      return state.filter((campus) => campus.id !== action.campus.id);
+    default:
+      return state;
+  }
+}
+
+export const deleteCampus = (campus) => {
+  return async (dispatch) => {
+    const { data: deleted } = await axios.delete(
+      `/api/campuses/${campus.id}`
+    );
+    dispatch(elDeleteCampus(deleted));
+  };
+};
+
+export const updateCampus = (campus) => {
+  return async (dispatch) => {
+    const { data: updated } = await axios.put(
+      `/api/campuses/${campus.id}`,
+      campus
+    );
+    dispatch(elUpdateCampus(updated));
+  };
+};
+
+export const createOneCampus = (campus) => {
+  return async (dispatch) => {
+    const { data: created } = await axios.post(
+      '/api/campuses',
+      campus
+    );
+    dispatch(elCreateOneCampus(created));
+  };
+};
+
+export const getCampuses = () => {
+  return async (dispatch) => {
+    const { data: campuses } = await axios.get('/api/campuses');
+    dispatch(elSetCampuses(campuses));
+  };
+};
+
+export default elCampusReducer;
